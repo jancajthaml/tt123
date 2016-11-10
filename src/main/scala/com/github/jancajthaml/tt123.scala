51419ebee85a32f74a44dba8beca8b3057455ed6
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-11-10 10:13:09 +0100

    definition of problem

--- src/main/scala/com/github/jancajthaml/tt123.scala
+++ src/main/scala/com/github/jancajthaml/tt123.scala
@@ -1,8 +1,61 @@
 package com.github.jancajthaml
 
 
+/**
+ * - Observe that this GNode can be thought of as defining a graph.
+ * - In implementing the functions below, you can assume that any graph defined by
+ *   a GNode is acyclic (no cycles).
+ * - Assume that when a GNode has no children, getChildren() returns an empty list,
+ *   and not null.
+ * - You can also assume that all children returned by getChildren() are not null.
+*/
 trait GNode {
 
   def getName: String
   def getChildren: List[GNode]
+}
+
+case class GNodeImpl(name: String, children: List[GNode] = List.empty[GNode]) extends GNode {
+
+  def getName: String = name
+
+  def getChildren: List[GNode] = children
+
+  /*
+    Implement a function with the following signature:
+      def walkGraph(node: GNode): List[GNode]
+
+    which should return a List containing every GNode in the graph.
+    Each node should appear in the List exactly once (i.e. no duplicates).
+  */
+  def walkGraph(node: GNode): List[GNode] = ???
+
+  /*
+    Implement a function with the following signature:
+      def paths(node: GNode): List[List[GNode]]
+
+    which should return a List of Lists, representing all possible paths
+    through the graph starting at 'node'.
+
+    The List returned can be thought of as a List of paths, where each
+    path is represented as an List of GNodes.
+  
+    Example:
+      Assume the following graph:
+
+       A
+         B
+           E
+           F
+         C
+           G
+           H
+           I
+         D
+           J
+
+    paths(A) = ( (A B E) (A B F) (A C G) (A C H) (A C I) (A D J) )
+  */
+  def paths(node: GNode): List[List[GNode]] = ???
+
 }
--- src/test/scala/com/github/jancajthaml/tt123/Specs.scala
+++ src/test/scala/com/github/jancajthaml/tt123/Specs.scala
@@ -4,5 +4,9 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Specs extends FlatSpec with Matchers {
 
+  "walkGraph" should "traverse each node exactly once (BFS traversal)" in {
+  }
 
+  "paths" should "find all paths from root to tail (DFS tuples)" in {
+  }
 }
